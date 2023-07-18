b3eb9fd3e9ffda648f4dc1324ec1d913bdd63845
Abi <abigail.feeley@hotmail.co.uk>
2023-07-18 16:23:16 +0100

    added 5.js

---
+++ Express/5.js
@@ -0,0 +1,27 @@
+let Express = require("express")
+let server = Express()
+let bodyParser = require("body-parser")
+
+server.use(bodyParser.json())
+server.use(bodyParser.urlencoded({extended: true}));
+server.post("/process", function(req, res){
+    let name = req.body.namelet salary = parseInt(req.body.sal)
+    let tax = 0
+    if (salary<1000){
+        tax = 0
+    }else if (salary>=1000 && salary<2000){
+        tax=salary*16/100
+    }else if (salary>=2000){
+        tax = salary*21/100
+    }
+
+    res.write("<h1> <center> SalarySlip of: ")
+    res.write(name)
+    res.write("</center> </h1>")
+    res.write("<br> Salary: " + salary)
+    res.write("<br> Tax: " + tax)
+
+    res.end()
+})
+
+server.listen(3000)
