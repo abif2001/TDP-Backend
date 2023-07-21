dd46a00096ccbf7e57077b888416040507475ff7
Abi <abigail.feeley@nationwide.co.uk>
2023-07-21 15:10:15 +0100

    added project 2

--- Express/Project 2/server.js
+++ Express/Project 2/server.js
@@ -6,9 +6,30 @@ let db = mySql.createConnection({
     host: "localhost",
     user:"root",
     password:"root",
-    database:"nbs"
+    database:"nbs2"
 })
 db.connect()
+
+server.get("/", function(req,res){
+    res.render("homepage.ejs")
+    res.end()
+})
+
+server.get("/allemployeeslist", function(req,res){
+    db.query("select * from employees"), function(err, records){
+        res.render("employeeslist.ejs", {employees:records})
+        res.end()
+    }
+})
+
+server.get("/departments", function(req,res){
+    db.query("select distinct department from employees"), function(err, records){
+        res.render("departments.ejs", {deptName:records})
+        res.end()
+    }
+})
+
+server.listen(3000)
 
 server.get("/show", function(req, res){
         db.query("select * from employees", function(err, data){
