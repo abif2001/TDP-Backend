ad4459f4dd8bdbd6cbf919e7332c70e148cd3633
Abi <abigail.feeley@hotmail.co.uk>
2023-07-28 11:38:22 +0100

    added app.js and showallemp

--- Complete project/App.js
+++ Complete project/App.js
@@ -1,42 +1,15 @@
-// import logo from './logo.svg';
+import logo from './logo.svg';
 import './App.css';
-// import Info from './info';
-// import Addition from './addition';
-// import Information from './Information';
-// import SalarySlip from './salarySlip';
-// import Results from './results';
-// import Children from './children';
-// import Numbers from './numbers';
-// import Records from './records';
-// import Events from './events';
-// import EmployeesData from './employees';
-
-import Data from './DataFromWeb';
+import ShowAllEmployees from './ShowAllEmployees';
+import NewEmployee from './NewEmployee';
 
 function App() {
   return (
-    <>
-      {/* <h1><center>Welcome to ReactJS training</center></h1>
-      <Info/>
-      <br/>
-      <Addition/>
-      <Information name="Shafeeq" address="Manchester"/>
-      <Information name="James Bond" address="MI6"/> */}
-      {/* <SalarySlip name="Shafeeq" salary="1500"/>
-      <SalarySlip name="James Bond" salary="10000"/> */}
-      {/* <Results name="James Bond" phy="75" chem="80" math="84"/>
-      <Results name="Matt" phy="65" chem="69" math="54"/>
-      <Results name="Phil" phy="55" chem="45" math="60"/> */}
-      {/* <Children>
-        Exam Results
-        </Children> */}
-        {/* <Numbers/> */}
-        <br/>
-        {/* <Records/> */}
-        {/* <Events/> */}
-        <Data/>
-    </>
-  );
+    <div>
+        <h1> hello </h1>
+        <NewEmployee/>
+        <ShowAllEmployees/>
+    </div>  );
 }
 
 export default App;
